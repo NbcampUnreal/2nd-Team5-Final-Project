b3bdaa0c288a40f22fa13c6b77180caa7cb8ff67
황찬호 <cksgh0316@gmail.com>
2025-05-07 17:34:25 +0900

    [Add] 케릭터 에셋 추가, BaseCharacter, Character 생성

---
+++ Source/StillLoading/Character/SLCharacter.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SLBaseCharacter.h"
+#include "SLCharacter.generated.h"
+
+UCLASS()
+class STILLLOADING_API ASLCharacter : public ASLBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASLCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
