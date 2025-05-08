4abd0c47907fdcfbc03a1699dc36ea33342b6329
igmiiii <sinker591@naver.com>
2025-05-08 21:00:08 +0900

    Reapply "Merge remote-tracking branch 'origin/dev' into 103-add-ai-character-shape-asset"
    This reverts commit 9e48114468d43a39626e26d58abe00fd188af90d.

--- Source/StillLoading/Character/SLBaseCharacter.h
+++ Source/StillLoading/Character/SLBaseCharacter.h
@@ -17,5 +17,4 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
