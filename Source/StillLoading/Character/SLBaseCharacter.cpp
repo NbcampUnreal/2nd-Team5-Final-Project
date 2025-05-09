aea66e131b4a5330f6581ec0fc8993e8643eb718
ChanhoHwang <cksgh0316@gmail.com>
2025-05-09 09:58:53 +0900

    [Add] 이동 처리 완료 디테일은 카메라 애니매이션 완료 후 수정 예정

--- Source/StillLoading/Character/SLBaseCharacter.cpp
+++ Source/StillLoading/Character/SLBaseCharacter.cpp
@@ -1,9 +1,20 @@
 #include "SLBaseCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 ASLBaseCharacter::ASLBaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
-	
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->TargetArmLength = 500.0f;
+	SpringArmComponent->bUsePawnControlRotation = true;
+
+	DefaultCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	DefaultCamera->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+	DefaultCamera->bUsePawnControlRotation = false;
 }
 
 void ASLBaseCharacter::BeginPlay()
