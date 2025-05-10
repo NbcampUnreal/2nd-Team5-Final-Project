094a94411fbbbe0dbd41dfb3e717026b785d4233
juchoi0632 <juchoi0632@gmail.com>
2025-05-10 16:04:44 +0900

    [Update] SLFadeWidget, WBP_SLFadeWidget

--- Source/StillLoading/UI/Widget/AdditiveWidget/SLFadeWidget.cpp
+++ Source/StillLoading/UI/Widget/AdditiveWidget/SLFadeWidget.cpp
@@ -2,4 +2,51 @@
 
 
 #include "UI/Widget/AdditiveWidget/SLFadeWidget.h"
+#include "Components/Image.h"
 
+void USLFadeWidget::InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType)
+{
+	WidgetType = ESLAdditiveWidgetType::FadeWidget;
+
+	OpenAnim = FadeInAnim;
+	CloseAnim = FadeOutAnim;
+
+	Super::InitWidget(NewUISubsystem, ChapterType);
+}
+
+void USLFadeWidget::ActivateWidget(ESLChapterType ChapterType)
+{
+	if (CurrentChapter != ChapterType)
+	{
+		ApplyOnChangedChapter(ChapterType);
+	}
+
+	if (bIsFadeIn)
+	{
+		PlayAnimation(OpenAnim);
+	}
+	else
+	{
+		PlayAnimation(CloseAnim);
+	}
+}
+
+void USLFadeWidget::DeactivateWidget()
+{
+	bIsFadeIn = !bIsFadeIn;
+}
+
+void USLFadeWidget::OnEndedOpenAnim()
+{
+	CloseWidget();
+}
+
+void USLFadeWidget::OnEndedCloseAnim()
+{
+	CloseWidget();
+}
+
+void USLFadeWidget::ApplyImageData()
+{
+	//FadeImage->SetBrushFromTexture();
+}
