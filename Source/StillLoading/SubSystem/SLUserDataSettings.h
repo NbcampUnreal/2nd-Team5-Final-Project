a9e589d9a760f1ab35ec358b0a844271c3f5b224
juchoi0632 <juchoi0632@gmail.com>
2025-05-10 23:29:21 +0900

    [Add]SLUserDataSettings, SLUserDataSubsystem. [Fix]SLKeyMappingWidget, SLKeySettingWidget, SLAdditiveWidget, SLOptionWidget, SLBaseWidget

---
+++ Source/StillLoading/SubSystem/SLUserDataSettings.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "UI/SLUITypes.h"
+#include "Character//DynamicIMCComponent/SLDynamicIMCComponent.h"
+#include "SLUserDataSettings.generated.h"
+
+class UInputMappingContext;
+
+UCLASS(Config = UserDataSettings, DefaultConfig, meta = (DisplayName = "UserDataSubsystemSettings"))
+class STILLLOADING_API USLUserDataSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultIMC")
+	TSoftObjectPtr<UInputMappingContext> PlayerIMC = nullptr;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultLanguageSettings")
+	ESLLanguageType DefaultLanguage = ESLLanguageType::EL_Kor;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultAudioSettings")
+	float DefaultBgmVolume = 1.0f;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultAudioSettings")
+	float DefaultEffectVolume = 1.0f;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultScreenSettings")
+	float DefaultBrightness = 1.0f;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultScreenSettings")
+	int32 DefaultWindowMode = 0;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultScreenSettings")
+	float DefaultScreenWidth = 1920.0f;
+
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultScreenSettings")
+	float DefaultScreenHeight = 1080.0f;
+	
+	UPROPERTY(EditAnywhere, Config, Category = "DefaultKeySettings")
+	TMap<EInputActionType, FKey> DefaultActionKeyMap;
+};
