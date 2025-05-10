b11e3aaa7e4a0b07127434c701475d61c0eb0eea
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2025-05-10 13:28:22 +0900

    [add] Pixelization & Outline with laplacian filter implement

---
+++ Source/StillLoading/Render/Shader/LSPixelizationShader.hlsl
@@ -0,0 +1,17 @@
+﻿float pixelSize = in_pixelSize;
+
+float2 viewportUv = GetDefaultSceneTextureUV(Parameters, 1);
+
+float2 viewportSize = View.ViewSizeAndInvSize.xy;
+float viewportRatio = viewportSize.y / viewportSize.x;
+float2 pixelViewportSize = pixelSize * float2(1.0, viewportRatio);
+
+float2 relativeUvToPixel = floor(viewportUv * pixelViewportSize) + float2(0.5, 0.5);
+float2 uv = relativeUvToPixel / pixelViewportSize;
+
+float2 texelSize = View.ViewSizeAndInvSize.zw;
+float2 halfTexelSize = texelSize * 0.5;
+
+uv = uv - fmod(uv, texelSize) + halfTexelSize;
+
+return SceneTextureLookup(uv, 14,false).xyz;
