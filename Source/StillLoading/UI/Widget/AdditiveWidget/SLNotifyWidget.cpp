a67b753ec5490ce40aa5ac07f8fe8793e4e3bfdd
juchoi0632 <juchoi0632@gmail.com>
2025-05-10 16:41:55 +0900

    [Update] SLNotifyWidget, WBP_SLNotifyWidget

--- Source/StillLoading/UI/Widget/AdditiveWidget/SLNotifyWidget.cpp
+++ Source/StillLoading/UI/Widget/AdditiveWidget/SLNotifyWidget.cpp
@@ -2,4 +2,45 @@
 
 
 #include "UI/Widget/AdditiveWidget/SLNotifyWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
 
+void USLNotifyWidget::InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType)
+{
+	WidgetType = ESLAdditiveWidgetType::NotifyWidget;
+
+	OpenAnim = VisibleNotifyAnim;
+	CloseAnim = InvisibleNotifyAnim;
+
+	Super::InitWidget(NewUISubsystem, ChapterType);
+}
+
+void USLNotifyWidget::DeactivateWidget()
+{
+	
+}
+
+void USLNotifyWidget::UpdateNotifyText(const FText& NewText)
+{
+	NotifyText->SetText(NewText);
+}
+
+void USLNotifyWidget::OnEndedOpenAnim()
+{
+	PlayAnimation(CloseAnim);
+}
+
+void USLNotifyWidget::OnEndedCloseAnim()
+{
+	CloseWidget();
+}
+
+void USLNotifyWidget::ApplyImageData()
+{
+	//BackgroundImg->SetBrushFromTexture();
+}
+
+void USLNotifyWidget::ApplyFontData()
+{
+	//NotifyText->SetFont();
+}
