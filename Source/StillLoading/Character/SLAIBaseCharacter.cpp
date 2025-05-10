e71c47c8d1d64cfbc25ffa94fdfb01d36042d145
spgray0426 <spgray@naver.com>
2025-05-10 13:10:09 +0900

    [Add] AI animation instance and Blackboard synchronization logic
    SLAICharacterAnimInstance.cpp 및 SLAICharacterAnimInstance.h 파일 추가로 AI 애니메이션 인스턴스 생성.
    BeginPlay에서 블랙보드와 AIController 초기화 로직 추가 (SLAIBaseCharacter).
    AIController의 OnAIPerceptionUpdated와 OnTargetPerceptionForgotten 함수에 블랙보드 연동 로직 추가.

--- Source/StillLoading/Character/SLAIBaseCharacter.cpp
+++ Source/StillLoading/Character/SLAIBaseCharacter.cpp
@@ -1,8 +1,24 @@
 #include "SLAIBaseCharacter.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Controller/SLBaseAIController.h"
+
 
 ASLAIBaseCharacter::ASLAIBaseCharacter()
 {
+}
+
+void ASLAIBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AIController = Cast<ASLBaseAIController>(GetController());
+    
+	if (AIController)
+	{        
+		BlackboardComp = AIController->GetBlackboardComponent();
+		UE_LOG(LogTemp, Warning, TEXT("블랙보드 초기화. The Name is : %s"), *BlackboardComp->GetName());        
+	}
 }
 
 
