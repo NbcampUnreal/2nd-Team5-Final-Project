205b07f048bf5142af19d9403b1d740a2de21bdc
juchoi0632 <juchoi0632@gmail.com>
2025-05-12 19:02:23 +0900

    [Add]SLMapElementWidget. [Update] SLMapListWidget

--- Source/StillLoading/UI/Widget/LevelWidget/SLMapListWidget.cpp
+++ Source/StillLoading/UI/Widget/LevelWidget/SLMapListWidget.cpp
@@ -3,3 +3,27 @@
 
 #include "UI/Widget/LevelWidget/SLMapListWidget.h"
 
+void USLMapListWidget::InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType)
+{
+	WidgetInputMode = ESLInputModeType::EIM_UIOnly;
+	bIsVisibleCursor = true;
+
+	Super::InitWidget(NewUISubsystem, ChapterType);
+}
+
+void USLMapListWidget::DeactivateWidget()
+{
+	Super::DeactivateWidget();
+
+	OnEndedCloseAnim();
+}
+
+void USLMapListWidget::ApplyImageData()
+{
+	Super::ApplyImageData();
+}
+
+void USLMapListWidget::ApplyFontData()
+{
+	Super::ApplyFontData();
+}
