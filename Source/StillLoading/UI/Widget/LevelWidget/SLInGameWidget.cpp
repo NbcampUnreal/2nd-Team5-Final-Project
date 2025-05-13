b51e89b358ff952f4edffd5be39225daa1d61417
juchoi0632 <juchoi0632@gmail.com>
2025-05-13 12:07:42 +0900

    [Update] SLInGameWidget

--- Source/StillLoading/UI/Widget/LevelWidget/SLInGameWidget.cpp
+++ Source/StillLoading/UI/Widget/LevelWidget/SLInGameWidget.cpp
@@ -3,3 +3,30 @@
 
 #include "UI/Widget/LevelWidget/SLInGameWidget.h"
 
+void USLInGameWidget::InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType)
+{
+	WidgetInputMode = ESLInputModeType::EIM_GameOnly;
+	bIsVisibleCursor = false;
+
+	Super::InitWidget(NewUISubsystem, ChapterType);
+
+}
+
+void USLInGameWidget::DeactivateWidget()
+{
+	Super::DeactivateWidget();
+
+	OnEndedCloseAnim();
+}
+
+void USLInGameWidget::ApplyImageData()
+{
+	Super::ApplyImageData();
+
+}
+
+void USLInGameWidget::ApplyFontData()
+{
+	Super::ApplyFontData();
+
+}
