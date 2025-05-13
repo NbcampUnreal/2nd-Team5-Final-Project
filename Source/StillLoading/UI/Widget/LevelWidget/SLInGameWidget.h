8ae4e68786c30c4b2041ded64dee3b6d65a18db7
juchoi0632 <juchoi0632@gmail.com>
2025-05-13 12:07:42 +0900

    [Update] SLInGameWidget

--- Source/StillLoading/UI/Widget/LevelWidget/SLInGameWidget.h
+++ Source/StillLoading/UI/Widget/LevelWidget/SLInGameWidget.h
@@ -6,12 +6,17 @@
 #include "UI/Widget/SLBaseWidget.h"
 #include "SLInGameWidget.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class STILLLOADING_API USLInGameWidget : public USLBaseWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType) override;
+	virtual void DeactivateWidget() override;
+
+protected:
+	virtual void ApplyImageData() override;
+	virtual void ApplyFontData() override;
 };
