121a1c342d3ee80cdb788de636b866aa49c10f0f
juchoi0632 <juchoi0632@gmail.com>
2025-05-13 16:14:43 +0900

    [Add] SLInGameHUD, BP_SLInGameHUD. [Update] SLInGameWidget, SLMapListWidget, WBP_SLInGameWidget

--- Source/StillLoading/UI/Widget/LevelWidget/SLInGameWidget.cpp
+++ Source/StillLoading/UI/Widget/LevelWidget/SLInGameWidget.cpp
@@ -3,6 +3,12 @@
 
 #include "UI/Widget/LevelWidget/SLInGameWidget.h"
 
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "Components/ProgressBar.h"
+#include "Components/CanvasPanel.h"
+#include "Animation/WidgetAnimation.h"
+
 void USLInGameWidget::InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType)
 {
 	WidgetInputMode = ESLInputModeType::EIM_GameOnly;
@@ -19,6 +25,65 @@ void USLInGameWidget::DeactivateWidget()
 	OnEndedCloseAnim();
 }
 
+void USLInGameWidget::SetIsTimerActivate(bool bIsActived)
+{
+	SetIsSubWidgetActivate(bIsActived, ActiveTimerAnim, DeactiveTimerAnim);
+}
+
+void USLInGameWidget::SetIsPlayerStateActivate(bool bIsActived)
+{
+	SetIsSubWidgetActivate(bIsActived, ActivePlayerStateAnim, DeactivePlayerStateAnim);
+}
+
+void USLInGameWidget::SetIsGameStateActivate(bool bIsActived)
+{
+	SetIsSubWidgetActivate(bIsActived, ActiveGameStateAnim, DeactiveGameStateAnim);
+}
+
+void USLInGameWidget::SetIsBossStateActivate(bool bIsActived)
+{
+	SetIsSubWidgetActivate(bIsActived, ActiveBossStateAnim, DeactiveBossStateAnim);
+}
+
+void USLInGameWidget::SetIsHitEffectActivate(bool bIsActived)
+{
+	if (bIsActived)
+	{
+		PlaySubWidgetAnim(ActiveHitEffectAnim);
+	}
+	else
+	{
+		HitEffectPanel->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+void USLInGameWidget::SetTimerText(int32 TimeSeconds)
+{
+	int32 TextMin = TimeSeconds / 60;
+	int32 TextSec = TimeSeconds % 60;
+
+	TimerText->SetText(FText::FromString(FString::Printf(TEXT("%2d : %2d"), TextMin, TextSec)));
+}
+
+void USLInGameWidget::SetHpValue(int32 MaxHp, int32 CurrentHp)
+{
+	float PerHp = (float)CurrentHp / (float)MaxHp;
+
+	HpBar->SetPercent(PerHp);
+}
+
+void USLInGameWidget::SetBossHpValue(int32 MaxHp, int32 CurrentHp)
+{
+	float PerHp = (float)CurrentHp / (float)MaxHp;
+
+	BossHpBar->SetPercent(PerHp);
+}
+
+void USLInGameWidget::SetGameStateText(const FText& StateText)
+{
+	GameStateText->SetText(StateText);
+}
+
 void USLInGameWidget::ApplyImageData()
 {
 	Super::ApplyImageData();
@@ -29,4 +94,32 @@ void USLInGameWidget::ApplyFontData()
 {
 	Super::ApplyFontData();
 
+}
+
+void USLInGameWidget::SetIsSubWidgetActivate(bool bIsActived, UWidgetAnimation* ActiveAnim, UWidgetAnimation* DeactiveAnim)
+{
+	if (bIsActived)
+	{
+		PlaySubWidgetAnim(ActiveAnim, DeactiveAnim);
+	}
+	else
+	{
+		PlaySubWidgetAnim(DeactiveAnim, ActiveAnim);
+	}
+}
+
+void USLInGameWidget::PlaySubWidgetAnim(UWidgetAnimation* PlayTargetAnim, UWidgetAnimation* StopTargetAnim)
+{
+	if (IsAnimationPlaying(PlayTargetAnim))
+	{
+		return;
+	}
+
+	if (IsValid(StopTargetAnim) && 
+		IsAnimationPlaying(StopTargetAnim))
+	{
+		StopAnimation(StopTargetAnim);
+	}
+
+	PlayAnimation(PlayTargetAnim);
 }
