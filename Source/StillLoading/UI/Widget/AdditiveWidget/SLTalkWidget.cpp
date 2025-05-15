4232c48d6ff6c17b2e3343bfb1f7a600e2395de4
juchoi0632 <juchoi0632@gmail.com>
2025-05-15 15:36:49 +0900

    [Update] SLTalkWidget, WBP_SLTalkWidget, SLUISubsystem, TextPoolSetting Config

--- Source/StillLoading/UI/Widget/AdditiveWidget/SLTalkWidget.cpp
+++ Source/StillLoading/UI/Widget/AdditiveWidget/SLTalkWidget.cpp
@@ -2,4 +2,138 @@
 
 
 #include "UI/Widget/AdditiveWidget/SLTalkWidget.h"
+#include "SubSystem/SLTextPoolSubsystem.h"
+#include "SubSystem/Struct/SLTextPoolDataRows.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/CanvasPanel.h"
 
+void USLTalkWidget::InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType)
+{
+	WidgetType = ESLAdditiveWidgetType::EAW_TalkWidget;
+	WidgetInputMode = ESLInputModeType::EIM_UIOnly;
+	bIsVisibleCursor = true;
+
+	Super::InitWidget(NewUISubsystem, ChapterType);
+
+	NextButton->OnClicked.AddDynamic(this, &ThisClass::OnClickedNextButton);
+}
+
+void USLTalkWidget::ActivateWidget(ESLChapterType ChapterType)
+{
+	Super::ActivateWidget(ChapterType);
+
+	TargetTextIndex = 0;
+	ChangeTargetText();
+}
+
+void USLTalkWidget::DeactivateWidget()
+{
+	Super::DeactivateWidget();
+
+	OnEndedCloseAnim();
+}
+
+void USLTalkWidget::UpdateTalkState(ESLTalkTargetType TalkTargetType, int32 TargetIndex)
+{
+	CheckValidOfTextPoolSubsystem();
+
+	const UDataTable* TalkDataTable = TextPoolSubsystem->GetTalkTextPool();
+
+	TArray<FSLTalkTextPoolDataRow*> TalkDataArray; 
+	TalkDataTable->GetAllRows(TEXT("Talk Text Context"), TalkDataArray);
+
+	for (const FSLTalkTextPoolDataRow* TalkData : TalkDataArray)
+	{
+		if (TalkData->TalkTarget == TalkTargetType &&
+			TalkData->TalkMap.Contains(TargetIndex))
+		{
+			TalkArray = TalkData->TalkMap[TargetIndex].TalkTextArray;
+			NameArray = TalkData->TalkMap[TargetIndex].TalkOwnArray;
+			CurrentTalkType = TalkTargetType;
+			CurrentTalkIndex = TargetIndex;
+			break;
+		}
+	}
+}
+
+void USLTalkWidget::ApplyImageData()
+{
+	Super::ApplyImageData();
+}
+
+void USLTalkWidget::ApplyFontData()
+{
+	Super::ApplyFontData();
+}
+
+void USLTalkWidget::ApplyTextData()
+{
+	Super::ApplyTextData();
+
+	if (!this->IsInViewport())
+	{
+		return;
+	}
+
+	GetWorld()->GetTimerManager().ClearTimer(TextPrintTimer);
+	UpdateTalkState(CurrentTalkType, CurrentTalkIndex);
+	ChangeTargetText();
+	PrintTalkText();
+}
+
+void USLTalkWidget::OnClickedNextButton()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TextPrintTimer);
+
+	if (CurrentTextIndex >= 0)
+	{
+		CurrentTextIndex = -1;
+		TalkText->SetText(TalkArray[TargetTextIndex]);
+		return;
+	}
+
+	++TargetTextIndex;
+
+	if (TargetTextIndex >= TalkArray.Num())
+	{
+		CloseWidget();
+		return;
+	}
+
+	ChangeTargetText();
+}
+
+void USLTalkWidget::PrintTalkText()
+{
+	if (CurrentTextIndex < 0)
+	{
+		return;
+	}
+
+	TalkText->SetText(FText::FromString(TargetText.ToString().LeftChop(CurrentTextIndex)));
+	--CurrentTextIndex;
+
+	GetWorld()->GetTimerManager().SetTimer(TextPrintTimer, this, &ThisClass::PrintTalkText, PrintTime, false);
+}
+
+void USLTalkWidget::ChangeTargetText()
+{
+	TalkText->SetText(FText::GetEmpty());
+
+	TargetText = TalkArray[TargetTextIndex];
+	CurrentTextIndex = TalkArray[TargetTextIndex].ToString().Len() - 1;
+
+	FName TargetName = NameArray[TargetTextIndex];
+
+	if (TargetName == "None")
+	{
+		NamePanel->SetVisibility(ESlateVisibility::Collapsed);
+		return;
+	}
+
+	NameText->SetText(FText::FromName(TargetName));
+	NamePanel->SetVisibility(ESlateVisibility::Visible);
+
+	PrintTalkText();
+}
--- Source/StillLoading/UI/Widget/AdditiveWidget/SLTalkWidget.h
+++ Source/StillLoading/UI/Widget/AdditiveWidget/SLTalkWidget.h
@@ -4,12 +4,77 @@
 
 #include "CoreMinimal.h"
 #include "UI/Widget/AdditiveWidget/SLAdditiveWidget.h"
+#include "UI/SLUITypes.h"
 #include "SLTalkWidget.generated.h"
 
+class UButton;
+class UTextBlock;
+class UImage;
+class UCanvasPanel;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMovedNextTalk, int32, TalkIndex);
 
 UCLASS()
 class STILLLOADING_API USLTalkWidget : public USLAdditiveWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void InitWidget(USLUISubsystem* NewUISubsystem, ESLChapterType ChapterType) override;
+	virtual void ActivateWidget(ESLChapterType ChapterType) override;
+	virtual void DeactivateWidget() override;
+
+	void UpdateTalkState(ESLTalkTargetType TalkTargetType, int32 TargetIndex);
+
+protected:
+	virtual void ApplyImageData() override;
+	virtual void ApplyFontData() override;
+	virtual void ApplyTextData() override;
+
+private:
+	UFUNCTION()
+	void OnClickedNextButton();
+
+	void PrintTalkText();
+	void ChangeTargetText();
+
+public:
+	UPROPERTY(EditAnywhere)
+	float PrintTime = 0.3f;
+
+	UPROPERTY()
+	FOnMovedNextTalk TalkDelegate;
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UButton> NextButton = nullptr;
+
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UTextBlock> NameText = nullptr;
+
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UTextBlock> TalkText = nullptr;
+
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UImage> TalkBack = nullptr;
+
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UImage> NameBack = nullptr;
+
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UCanvasPanel> NamePanel = nullptr;
+
+
+	UPROPERTY()
+	TArray<FName> NameArray;
+
+	UPROPERTY()
+	TArray<FText> TalkArray;
+
+	FTimerHandle TextPrintTimer;
+	FText TargetText = FText::GetEmpty();
+	ESLTalkTargetType CurrentTalkType = ESLTalkTargetType::ETT_None;
+	int32 CurrentTalkIndex = 0;
+	int32 TargetTextIndex = 0;
+	int32 CurrentTextIndex = -1;
 };
