4e567f009230cb75354a894c7400959606603ca1
황찬호 <cksgh0316@gmail.com>
2025-05-15 14:59:35 +0900

    [Add] Radar 구현 Tick -> Timer 기반으로 변경

--- Source/StillLoading/Controller/SLBasePlayerController.cpp
+++ Source/StillLoading/Controller/SLBasePlayerController.cpp
@@ -1,25 +1,8 @@
 #include "SLBasePlayerController.h"
 
-#include "Blueprint/UserWidget.h"
-#include "Character/SLBaseCharacter.h"
-
 void ASLBasePlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	if (InputWidgetClass)
-	{
-		InputWidget = CreateWidget<UUserInputWidget>(this, InputWidgetClass);
-
-		if (InputWidget)
-		{
-			InputWidget->AddToViewport();
-
-			InputWidget->LinkedCharacter = Cast<ASLBaseCharacter>(GetPawn());
-
-			bShowMouseCursor = false;
-		}
-	}
 
 	//팀 번호 설정
 	playerTeamId = FGenericTeamId(0);
