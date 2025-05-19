298a0438012bc9fbd350ab331571b1a3e7f4a752
황찬호 <cksgh0316@gmail.com>
2025-05-19 17:53:23 +0900

    [Add] 배틀 컴포넌트 커뮤니케이션 완료, 트레이스 채널 추가

--- Source/StillLoading/Character/SLAIBaseCharacter.cpp
+++ Source/StillLoading/Character/SLAIBaseCharacter.cpp
@@ -40,6 +40,9 @@ ASLAIBaseCharacter::ASLAIBaseCharacter()
 	// 카메라 채널 무시
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+
+	// BattleComponent 에서 사용 하기위한 캡슐 셋팅
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_GameTraceChannel1, ECR_Overlap);
 }
 
 void ASLAIBaseCharacter::BeginPlay()
