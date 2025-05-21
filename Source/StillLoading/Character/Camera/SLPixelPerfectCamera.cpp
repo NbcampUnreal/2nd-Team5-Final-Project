a81e8c928cbee05b749dbabd183a2af3946acde8
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-05-21 16:05:47 +0900

    [Add] 2D movemnet, 카메라 구현 및 리디렉션 레퍼런스 삭제

---
+++ Source/StillLoading/Character/Camera/SLPixelPerfectCamera.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SLPixelPerfectCamera.h"
+
+USLPixelPerfectCamera::USLPixelPerfectCamera()
+{
+	bConstrainAspectRatio = true;
+	AspectRatio = 16.0f / 9.0f;
+}
+
+void USLPixelPerfectCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	OwnerActor = GetOwner();
+
+	InitPixelSize();
+	SetTickGroup(TG_PostPhysics);
+}
+
+void USLPixelPerfectCamera::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (IsValid( OwnerActor))
+	{
+		FVector TargetLocation = OwnerActor->GetActorLocation();
+		FVector CurrentSnapLocation = FVector::ZeroVector;
+		
+		CurrentSnapLocation.X = FMath::GridSnap(TargetLocation.X, PixelSize.X);
+		CurrentSnapLocation.Y = FMath::GridSnap(TargetLocation.Y, PixelSize.Y);
+		SetWorldLocation(CurrentSnapLocation);
+	}
+}
