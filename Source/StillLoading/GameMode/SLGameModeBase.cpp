f609d2ec2dd8eb4bf97a84523b23bba5fca37a50
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-05-30 16:44:51 +0900

    [add] Game flow base class

---
+++ Source/StillLoading/GameMode/SLGameModeBase.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SLGameModeBase.h"
+
+#include "GameState/SLGameStateBase.h"
+
+ASLGameModeBase::ASLGameModeBase()
+{
+}
+
+void ASLGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	SLGameState = Cast<ASLGameStateBase>(GameState);
+	checkf(SLGameState, TEXT("GameState is not ASLGameStateBase"));
+}
+
+void ASLGameModeBase::LoadGame()
+{
+	
+}
+
+void ASLGameModeBase::StartGameSubject(USLGameSubjectBase* Subject)
+{
+	SLGameState->AddGameSubject(Subject);
+	OnGameSubjectStartedDelegate.Broadcast(Subject);
+}
+
+void ASLGameModeBase::EndGameSubject(USLGameSubjectBase* Subject)
+{
+	SLGameState->RemoveGameSubject(Subject);
+	OnGameSubjectFinishedDelegate.Broadcast(Subject);
+}
