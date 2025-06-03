136e5a2a93c1a63cc96954c4ebcf31e8fc77b8cf
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-06-03 21:56:35 +0900

    [add] ObjectiveHandlerBase 기능 및 이벤트 추가
    - ObjectiveHandlerBase에 새로운 기능과 Delegate 이벤트 추가
    - Objective 상태별 처리 로직 구체화 (완료, 진행 중, 실패, 대기)
    - 목표 상태 변경 및 진행 상황 업데이트 기능 구현
    - Objective와의 초기화 및 연결 로직 추가

--- Source/StillLoading/Objective/SLObjectiveHandlerBase.h
+++ Source/StillLoading/Objective/SLObjectiveHandlerBase.h
@@ -7,27 +7,72 @@
 #include "SLObjectiveHandlerBase.generated.h"
 
 
+class USLObjectiveBase;
+enum class ESLChapterType : uint8;
 enum class ESLObjectiveState : uint8;
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class USLObjectiveHandlerBase;
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FOnObjectiveCompletedSignature, USLObjectiveHandlerBase, OnObjectiveCompletedDelegate, USLObjectiveHandlerBase*, Component);
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FOnObjectiveInProgressedSignature, USLObjectiveHandlerBase, OnObjectiveInProgressedDelegate, USLObjectiveHandlerBase*, Component);
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FOnObjectiveFailedSignature, USLObjectiveHandlerBase, OnObjectiveFailedDelegate, USLObjectiveHandlerBase*, Component);
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FOnObjectiveStandbySignature, USLObjectiveHandlerBase, OnObjectiveStandbyDelegate, USLObjectiveHandlerBase*, Component);
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_OneParam(FOnObjectiveProgressUpdatedSignature, USLObjectiveHandlerBase, OnObjectiveProgressUpdatedDelegate, int32, ProgressCount);
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class STILLLOADING_API USLObjectiveHandlerBase : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:
 	USLObjectiveHandlerBase();
-
-public:
-	UFUNCTION()
-	virtual void OnObjectiveStateChanged(ESLObjectiveState ObjectiveState);
-
 	
+	UFUNCTION(BlueprintCallable, Category = "ObjectiveHandler")
+	USLObjectiveBase* GetObjective();
+
 protected:
 	virtual void BeginPlay() override;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TArray<FName> ObjectiveList;
+	UFUNCTION(BlueprintNativeEvent, Category = "ObjectiveHandler")
+	void OnObjectiveCompleted();
+	virtual void OnObjectiveCompleted_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, Category = "ObjectiveHandler")
+	void OnObjectiveInProgressed();
+	virtual void OnObjectiveInProgressed_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, Category = "ObjectiveHandler")
+	void OnObjectiveFailed();
+	virtual void OnObjectiveFailed_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, Category = "ObjectiveHandler")
+	void OnObjectiveStandby();
+	virtual void OnObjectiveStandby_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, Category = "ObjectiveHandler")
+	void OnObjectiveProgressUpdated(const int32 ProgressCount);
+	virtual void OnObjectiveProgressUpdated_Implementation(const int32 ProgressCount);
 	
 private:
-	void SubscribeObjectives();
+	UFUNCTION()
+	void OnObjectiveStateChanged(const ESLObjectiveState ObjectiveState);
+
+	void InitializeObjective();
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="ObjectiveHandler")
+	FOnObjectiveCompletedSignature OnObjectiveCompletedDelegate;
+	UPROPERTY(BlueprintAssignable, Category="ObjectiveHandler")
+	FOnObjectiveInProgressedSignature OnObjectiveInProgressedDelegate;
+	UPROPERTY(BlueprintAssignable, Category="ObjectiveHandler")
+	FOnObjectiveFailedSignature OnObjectiveFailedDelegate;
+	UPROPERTY(BlueprintAssignable, Category="ObjectiveHandler")
+	FOnObjectiveStandbySignature OnObjectiveStandbyDelegate;
+	UPROPERTY(BlueprintAssignable, Category="ObjectiveHandler")
+	FOnObjectiveProgressUpdatedSignature OnObjectiveProgressUpdatedDelegate;
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "ObjectiveHandler")
+	ESLChapterType Chapter;
+	UPROPERTY(EditAnywhere, Category = "ObjectiveHandler")
+	FName ObjectiveName;
 };
