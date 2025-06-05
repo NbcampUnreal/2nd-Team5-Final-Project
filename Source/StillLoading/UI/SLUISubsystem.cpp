ef7ce36c034b1fcc2e1245d1cbd745d43d50dc2e
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-06-06 02:15:26 +0900

    [add] 애니메이션 프레임 레이트 제한 및 레벨 수정

--- Source/StillLoading/UI/SLUISubsystem.cpp
+++ Source/StillLoading/UI/SLUISubsystem.cpp
@@ -89,7 +89,7 @@ void USLUISubsystem::ActivateStory(ESLStoryType TargetStoryType, const FName& St
 	AddAdditiveWidget(ESLAdditiveWidgetType::EAW_StoryWidget);
 }
 
-void USLUISubsystem::ActivateTalk(ESLTalkTargetType TalkTargetType, const FName& TargetName, const FName& TalkName)
+void USLUISubsystem::ActivateTalk(ESLTalkTargetType TalkTargetType, FName TargetName, FName TalkName)
 {
 	WidgetActivateBuffer.TargetTalk = TalkTargetType;
 	WidgetActivateBuffer.TargetName = TargetName;
