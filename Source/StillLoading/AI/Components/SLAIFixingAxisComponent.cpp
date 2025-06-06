ed1f75451c0448ed8b65d79a933b9c2c0641f99d
spgray0426 <spgray@naver.com>
2025-06-06 22:34:08 +0900

    [Add] SLAIFixingAxisComponent 추가
    AI의 축 방향 수정 컴포넌트(SLAIFixingAxisComponent) 추가
    
    - USLAIFixingAxisComponent 클래스와 헤더 파일 생성 및 구현
      - AI의 이동 방향에 따라 SkeletalMeshComponent의 축을 정렬하도록 기능 추가
      - CharacterMovementComponent와 SkeletalMeshComponent 가져와 이동 벡터를 기반으로 회전값 계산 후 적용
    - BP_Companion 및 관련 액터들에 SLAIFixingAxisComponent 추가
    - 챕터 0 보스 스테이지 맵 업데이트 (관련 컴포넌트 적용)

---
+++ Source/StillLoading/AI/Components/SLAIFixingAxisComponent.cpp
@@ -0,0 +1,46 @@
+
+#include "SLAIFixingAxisComponent.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+USLAIFixingAxisComponent::USLAIFixingAxisComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void USLAIFixingAxisComponent::BeginPlay()
+{
+    Super::BeginPlay();
+    UActorComponent* MovementActorComponent = GetOwner()->GetComponentByClass(UCharacterMovementComponent::StaticClass());
+    UActorComponent* SkeletalMeshActorComponent = GetOwner()->GetComponentByClass(USkeletalMeshComponent::StaticClass());
+    check(MovementActorComponent);
+    check(SkeletalMeshActorComponent);
+    MovementComponent = Cast<UCharacterMovementComponent>(MovementActorComponent);
+    SkeletalMeshComponent = Cast<USkeletalMeshComponent>(SkeletalMeshActorComponent);
+    
+}
+
+void USLAIFixingAxisComponent::TickComponent(float DeltaTime, ELevelTick TickType,FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    const FVector& Velocity = MovementComponent->GetVelocityForNavMovement();
+    if (Velocity.IsNearlyZero())
+    {
+        return;
+    }
+    
+    const FVector UnitVelocity = Velocity.GetSafeNormal();
+
+    const float XRotation = FMath::RadiansToDegrees(FMath::Atan2(UnitVelocity.Z, FMath::Sqrt(FMath::Square(UnitVelocity.X) + FMath::Square(UnitVelocity.Y))));
+    const float YRotation = FMath::RadiansToDegrees(FMath::Atan2(UnitVelocity.Y, UnitVelocity.X)) - 90.0f;
+
+    UE_LOG(LogTemp, Warning, TEXT("[USLAIFixingAxisComponent::TickComponent] : X  %f, Y : %f "), XRotation, YRotation );
+
+    
+    const float RoundedYRotation = FMath::RoundToFloat(YRotation / 90.0f) * 90.0f;
+    SkeletalMeshComponent->SetRelativeRotation(FRotator(0.0f, RoundedYRotation, 0.0f));
+}
+
