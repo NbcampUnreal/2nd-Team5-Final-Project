7216e474a831cc99be2bd28c3c997624e0e63dd5
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-06-10 19:04:53 +0900

    [delete] SLNPC 관련 코드 및 사용되지 않는 에셋 제거
    - SLNPC.cpp, SLNPC.h 삭제
    - 관련 에셋 (BP_SLNPC, ABP_SLNPC, BT_SLNPC 등) 삭제
    - SL2DMovementHandlerComponent에서 SLNPC 관련 종속성 제거
    - Interactable Object 및 관련 캐릭터 시스템 추가
    - Interaction 액션 및 Radar 기반 상호작용 로직 구현

---
+++ Source/StillLoading/Objective/Interactables/SLInteractableCharacter.cpp
@@ -0,0 +1,29 @@
+﻿#include "SLInteractableCharacter.h"
+
+#include "UI/SLUISubsystem.h"
+
+ASLInteractableCharacter::ASLInteractableCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CharacterMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh"));
+	CharacterMesh->SetupAttachment(RootComponent);
+}
+
+void ASLInteractableCharacter::Interaction()
+{
+	ActivateCurrentTalk();
+}
+
+void ASLInteractableCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	UISubsystem = GetGameInstance()->GetSubsystem<USLUISubsystem>();
+	check(UISubsystem)
+}
+
+void ASLInteractableCharacter::ActivateCurrentTalk()
+{
+	UISubsystem->ActivateTalk(ESLTalkTargetType::ETT_NPC, CurrentTargetName, CurrentTalkName);
+}
+
---
+++ Source/StillLoading/Objective/Interactables/SLInteractableObjectBase.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SLInteractableObjectBase.generated.h"
+
+UINTERFACE()
+class USLInteractableObjectBase : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class STILLLOADING_API ISLInteractableObjectBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Interaction() = 0;
+};
