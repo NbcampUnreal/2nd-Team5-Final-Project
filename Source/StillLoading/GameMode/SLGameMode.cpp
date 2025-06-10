12e74787d44e17dd5054f01ff7bcd090f6a5795e
황찬호 <cksgh0316@gmail.com>
2025-06-10 21:01:35 +0900

    [Add] 2.5D 케릭터 추가, 반투명 오버랩 박스 추가, 게임모드 반투명화 추가

--- Source/StillLoading/GameMode/SLGameMode.cpp
+++ Source/StillLoading/GameMode/SLGameMode.cpp
@@ -3,3 +3,32 @@
 
 #include "SLGameMode.h"
 
+#include "Kismet/GameplayStatics.h"
+
+void ASLGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> AllActors;
+    
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AActor::StaticClass(), AllActors);
+
+	for (AActor* Actor : AllActors)
+	{
+		if (!Actor)
+		{
+			continue;
+		}
+
+		TArray<UPrimitiveComponent*> PrimitiveComponents;
+		Actor->GetComponents<UPrimitiveComponent>(PrimitiveComponents);
+
+		for (UPrimitiveComponent* PrimComp : PrimitiveComponents)
+		{
+			if (PrimComp)
+			{
+				PrimComp->SetGenerateOverlapEvents(true);
+			}
+		}
+	}
+}
