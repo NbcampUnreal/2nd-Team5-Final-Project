c0aad49a73dd58b12bb50692ebf81b2e8041bd86
spgray0426 <spgray@naver.com>
2025-06-17 22:26:09 +0900

    [Update] 마우스 액터 로직 수정

--- Source/StillLoading/AI/Actors/SLMouseActor.h
+++ Source/StillLoading/AI/Actors/SLMouseActor.h
@@ -17,7 +17,6 @@ enum class EMouseActorState : uint8
 {
 	Inactive	UMETA(DisplayName = "Inactive"),
 	Chasing		UMETA(DisplayName = "Chasing"),
-	Attacking	UMETA(DisplayName = "Attacking"),
 	Grabbing	UMETA(DisplayName = "Grabbing"),
 	Destroyed	UMETA(DisplayName = "Destroyed")
 };
@@ -40,34 +39,16 @@ public:
 	void DestroyMouseActor();
 
 	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	void GrabAndMovePlayer(const FVector& NewLocation);
-
-	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	void SetChaseSpeed(float NewSpeed);
-
-	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	void SetAttackDamage(float NewDamage);
-
-	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
 	EMouseActorState GetCurrentState() const;
 
 	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
 	void TakeDamage(float DamageAmount);
 
 	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	void EnablePeriodicGrab(bool bEnable);
+	void SetChaseSpeed(float NewSpeed);
 
 	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	void SetGrabSettings(float MinInterval, float MaxInterval, float GrabChance, float GrabRadius);
-
-	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	void SetDebugMode(bool bEnable);
-
-	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	FVector GetPlayerDirection() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Mouse Actor")
-	float GetDistanceToPlayer() const;
+	void SetGrabSettings(float NewGrabDistance, float NewGrabHeight, float NewGrabDamage, float NewGrabDuration, float NewGrabCooldown);
 
 	UPROPERTY(BlueprintAssignable, Category = "Mouse Actor")
 	FSLOnMouseActorDestroyed OnMouseActorDestroyed;
@@ -83,18 +64,16 @@ protected:
 	void OnBattleComponentHit(AActor* DamageCauser, float DamageAmount, const FHitResult& HitResult, EHitAnimType HitAnimType);
 
 	void UpdateChaseTarget();
-	void ChasePlayer();
-	void AttackPlayer();
-	void UpdateThreatEffects();
-	void PlayDestroyEffects();
-	void SetMouseActorState(EMouseActorState NewState);
 	void MoveTowardsPlayer(float DeltaTime);
-	void StartPeriodicGrab();
-	void StopPeriodicGrab();
-	void ExecutePeriodicGrab();
-	FVector GetRandomGrabLocation();
-	bool IsSafeGrabLocation(const FVector& Location);
-	void ScheduleNextGrab();
+	void MoveTowardsGrabTarget(float DeltaTime);
+	void UpdateMeshRotation(float DeltaTime);
+	void StartGrabPlayer();
+	void CompleteGrabPlayer();
+	void ApplyGrabDamage();
+	void SetMouseActorState(EMouseActorState NewState);
+	ASLPlayerCharacter* FindPlayerCharacter();
+	bool IsPlayerInRange() const;
+	bool CanGrabPlayer() const;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<USceneComponent> RootSceneComponent;
@@ -112,101 +91,47 @@ protected:
 	float ChaseSpeed;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float AttackDamage;
+	float GrabDistance;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float AttackRange;
+	float GrabHeight;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float GrabForce;
+	float GrabDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
+	float GrabDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
+	float GrabCooldown;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
 	float MaxHealth;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float ThreatEffectInterval;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float RotationSpeed;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float AttackCooldown;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	bool bEnablePeriodicGrab;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float GrabIntervalMin;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float GrabIntervalMax;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float GrabProbability;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float GrabLocationRadius;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float MinGrabDistance;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mouse Settings")
-	float MaxGrabDistance;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
-	bool bShowDebugInfo;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
-	bool bDrawDebugLines;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
-	float DebugLineThickness;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
-	float DebugLineDuration;
+	float DetectionRange;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
-	TObjectPtr<UNiagaraSystem> ChaseEffect;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
-	TObjectPtr<UNiagaraSystem> AttackEffect;
+	TObjectPtr<UNiagaraSystem> GrabEffect;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
 	TObjectPtr<UNiagaraSystem> DestroyEffect;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
-	TObjectPtr<UNiagaraSystem> ThreatEffect;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
-	TObjectPtr<USoundBase> ChaseSound;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
-	TObjectPtr<USoundBase> AttackSound;
+	TObjectPtr<USoundBase> GrabSound;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects")
 	TObjectPtr<USoundBase> DestroySound;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials")
-	TObjectPtr<UMaterialInterface> NormalMaterial;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials")
-	TObjectPtr<UMaterialInterface> ThreatMaterial;
-
+	
 private:
-	ASLPlayerCharacter* FindPlayerCharacter();
-	bool IsPlayerInAttackRange();
-	void HandlePlayerGrabbing();
-	void ResetAttackCooldown();
-
 	EMouseActorState CurrentState;
 	float CurrentHealth;
 	TObjectPtr<ASLPlayerCharacter> TargetPlayer;
-	FTimerHandle ThreatEffectTimerHandle;
-	FTimerHandle AttackCooldownHandle;
-	FTimerHandle PeriodicGrabTimerHandle;
-	FVector LastPlayerLocation;
-	FVector TargetLocation;
-	bool bCanAttack;
-	bool bIsGrabbing;
-	bool bPeriodicGrabActive;
+	FVector GrabStartLocation;
+	FVector GrabTargetLocation;
+	FTimerHandle GrabTimerHandle;
+	FTimerHandle GrabCooldownTimerHandle;
+	bool bCanGrab;
+	
 };
