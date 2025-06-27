da7d9a0df5e93659e67eb71e416bc453ccc5a52f
안장훈 <82528291+wkdgns135@users.noreply.github.com>
2025-06-27 12:58:25 +0900

    [add] 캐릭터 디버그 선 표시 기능 및 관련 치트 추가
    - `USLInteractionComponent`에 디버그 선 표시 메서드 추가 (`SetDebugLineVisibility`)
    - `USLCheatManager`에 디버그 선 표시 명령 추가 (`ShowCharacterDebugLine`)
    - `L_Ch1_GameMap4` 맵 파일 업데이트

--- Source/StillLoading/Cheat/SLCheatManager.cpp
+++ Source/StillLoading/Cheat/SLCheatManager.cpp
@@ -3,6 +3,9 @@
 
 #include "SLCheatManager.h"
 
+#include "Character/BattleComponent/BattleComponent.h"
+#include "Character/Interaction/SLInteractionComponent.h"
+#include "GameFramework/Character.h"
 #include "GameMode/SLGameModeBase.h"
 #include "Kismet/GameplayStatics.h"
 #include "Objective/SLObjectiveBase.h"
@@ -26,6 +29,21 @@ void USLCheatManager::AddCurrentObjectiveProgress()
 		if (USLObjectiveBase* Objective = GameMode->GetPrimaryInProgressObjective())
 		{
 			Objective->AddObjectiveProgress();
+		}
+	}
+}
+
+void USLCheatManager::ShowCharacterDebugLine(const bool bFlag)
+{
+	if (const ACharacter* Character = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0))
+	{
+		if (USLInteractionComponent* InteractionComponent = Character->GetComponentByClass<USLInteractionComponent>()){
+			InteractionComponent->SetDebugLineVisibility(bFlag);
+		}
+
+		if (UBattleComponent *BattleComponent = Character->GetComponentByClass<UBattleComponent>())
+		{
+			BattleComponent->bShowDebugLine = bFlag;
 		}
 	}
 }
