34f824416df2ea40b69d2587cd3ebdb06d1365bc
spgray0426 <spgray@naver.com>
2025-08-03 00:27:33 +0900

    [Add] 챕터 3 미니게임3 오브젝트
    숨을때 오브젝트

---
+++ Source/StillLoading/Interactable/Object/SLInteractableObjectAnimation.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable/SLInteractableObjectBase.h"
+#include "SLInteractableObjectAnimation.generated.h"
+
+class UAnimMontage;
+class UArrowComponent;
+
+UCLASS()
+class STILLLOADING_API ASLInteractableObjectAnimation : public ASLInteractableObjectBase
+{
+	GENERATED_BODY()
+	
+public:
+	ASLInteractableObjectAnimation();
+
+protected:
+	virtual void OnInteracted(const ASLPlayerCharacterBase* InCharacter, ESLReactiveTriggerType InTriggerType) override;
+
+	UFUNCTION()
+	void ExecuteEnterAnimation(ASLPlayerCharacterBase* InCharacter);
+
+	UFUNCTION()
+	void ExecuteExitAnimation(ASLPlayerCharacterBase* InCharacter);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Hiding")
+	void OnPlayerEnterHiding(ASLPlayerCharacterBase* InCharacter);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Hiding")
+	void OnPlayerExitHiding(ASLPlayerCharacterBase* InCharacter);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UArrowComponent> EnterTransformArrow;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	TObjectPtr<UAnimMontage> EnterMontage;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	TObjectPtr<UAnimMontage> ExitMontage;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State")
+	bool bIsPlayerHiding;
+};
